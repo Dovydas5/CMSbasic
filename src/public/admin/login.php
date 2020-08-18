0f2a2abdd5d2656ed5b88d8256ba148e361e74b9
pretendent <dovydaspacesa@gmail.com>
2020-08-18 12:37:04 +0300

    login added

--- src/public/admin/login.php
+++ src/public/admin/login.php
@@ -1,39 +1,36 @@
 <?php
 require '../../app/start.php';
 
-if(isset($_POST['submit-btn'])) {
 
+//$stmt = $db->prepare('INSERT INTO users (username, password) VALUES (:username, :password)');
+//$stmt->execute(array(
+//    "username" => "admin",
+//    "password" => $hashedPwd));
+if (isset($_POST['submit-btn'])) {
 
 
     $username = $_POST['uid'];
-    $password = $_POST['pwd'];
-    $password = md5($password);
+    $password = 'admin';
+    $hashedPwd = password_hash($password, PASSWORD_BCRYPT, array('cost' => 10));
 
-    try{
-        $SQLQuery = "SELECT * FROM users WHERE username = :username";
-        $stmt = $db->prepare($SQLQuery);
-        $stmt->execute(array(':username'=>$username));
+    $SQLQuery = "SELECT * FROM users WHERE username = :username";
+    $stmt = $db->prepare($SQLQuery);
+    $stmt->execute(array(':username' => $username));
 
-        while ($row = $stmt ->fetch()){
-            $id = $row['id'];
-            $hashedPwd = $row['password'];
-            $username = $row['username'];
+    while ($row = $stmt->fetch()) {
+        $id = $row['id'];
+        $hashedPwd = $row['password'];
+        $username = $row['username'];
 
 
-            if(password_verify($password, $hashedPwd)){
-                $_SESSION['id'] = $id;
-                $_SESSION['username'] = $username;
-                header('Location: index.php');
-            }
-            else {
-                echo "error:psw arba username";
-            }
-
+        if (password_verify($password, $hashedPwd)) {
+            $_SESSION['id'] = $id;
+            $_SESSION['username'] = $username;
+            header('Location: index.php');
+        } else {
+            echo "<h1>Wrong username or password</h1>";
         }
-    }
-    catch (PDOException $e){
-        echo "error: " . $e->getMessage();
-    }
 
+    }
 }
 require VIEW_ROOT . '/admin/login.php';
